e56a200ca02f5236ed77bde0b6e12327920064ab
Isabel Mate <isabelmate@gmail.com>
2018-02-19 23:39:01 +0100

    Watermark para text area, show button para opción volver, datepicker con formato

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+showButton();
+
 //para radio button en el accordion
 $("body").on("click", ".radioPayment", function(){
   $(this).parent().find("a").trigger("click")
@@ -13,4 +15,24 @@ $("body").on("change", "#acceptPolicy", function() {
     $("#sendFormButton").parent().addClass("disabled");
     $("#sendFormButton").prop( "disabled", true );
   }
+});
+
+function showButton() {
+ var str = window.location.href
+ var n = str.search("#volver");
+ if (n != -1) {
+   $("#buttonBack").show();
+ } else {
+   $("#buttonBack").hide();
+ }
+}
+
+//Plugin from https://bootstrap-datepicker.readthedocs.io/en/stable/
+
+$('input[name="expirationDate"]').datepicker({
+  format: 'mm/yyyy',
+  todayHighlight: true,
+  autoclose: true,
+  maxViewMode: 1,
+  minViewMode: 1
 });
