f2bbee15f4bb2a5f6406da0db0d419f9aa08b541
isabelmate <36552251+isabelmate@users.noreply.github.com>
2018-02-20 10:04:33 +0100

    Corrección input-addon-group en errores validación

--- assets/js/validateForm.js
+++ assets/js/validateForm.js
@@ -1,3 +1,20 @@
+$.validator.setDefaults({
+          highlight: function(element) {
+            $(element).closest(".form-group").addClass("has-error");
+        },
+        unhighlight: function(element) {
+            $(element).closest(".form-group").removeClass("has-error");
+        },
+    errorElement: "label",
+    errorClass: "error",
+    errorPlacement: function(error, element) {
+        if(element.parent(".input-group").length) {
+            error.insertAfter(element.parent());
+        } else {
+            error.insertAfter(element);
+        }
+    }
+});
 
 $("#formPayment").validate({
 
